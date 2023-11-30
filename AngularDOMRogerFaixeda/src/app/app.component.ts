52cad5b977aa9104f9730a48d0b19a92b0680d48
Roger Faixeda <roger.faixeda@institutvidreres.cat>
2023-11-30 11:50:32 +0100

    ex2

--- AngularDOMRogerFaixeda/src/app/app.component.ts
+++ AngularDOMRogerFaixeda/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CompRogerComponent} from "./comp-roger/comp-roger.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CompRogerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- AngularDOMRogerFaixeda/src/app/comp-roger/comp-roger.component.ts
+++ AngularDOMRogerFaixeda/src/app/comp-roger/comp-roger.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-comp-roger',
@@ -8,6 +8,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './comp-roger.component.html',
   styleUrl: './comp-roger.component.css'
 })
-export class CompRogerComponent {
-
+export class CompRogerComponent implements OnInit{
+  constructor() {
+  }
+  ngOnInit() {
+    this.fun1();
+    this.fun2();
+  }
+  fun1 () {
+    let titolMayus = document.getElementById('titol')!;
+    titolMayus.innerHTML = titolMayus.innerHTML.toUpperCase();
+  }
+  fun2 (){
+    return document.location.href;
+  }
 }
